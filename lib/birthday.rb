1efa67b7e47826dffff14ca6a2a47243fa83676a
Cody Toney-Griffith <cody.toney@gmail.com>
2019-12-22 16:33:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,6 +7,8 @@ birthday_kids = {
 def happy_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    while age <= 12 
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end # add your code snippet here!
 end
 
